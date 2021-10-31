ddb7531f80d3aa3a93340d29e8ca474dedfcdb7a
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-11-01 01:12:09 +0500

    add style/ components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css'
+import SettingsBar from './components/SettingsBar';
+import Toolbar from './components/ToolBar';
+import Canvas from './components/Canvas';
+import './styles/app.scss'
 
 function App() {
   return (
-    
+    <div className="App">
+      <Toolbar />
+      <SettingsBar />
+      <Canvas />
+    </div>
   )
 }
 
---
+++ src/components/SettingsBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/toolbar.scss'
+
+const SettingsBar = () => {
+    return (
+        <div className='settingsBar'>
+            
+        </div>
+    );
+};
+
+export default SettingsBar;
