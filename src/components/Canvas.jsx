cc152acc826ca9d36bc7707e2f15911ce63c077e
vladislavkostromin <61909342+vladislavkostromin@users.noreply.github.com>
2021-11-07 14:03:59 +0500

    brush

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite';
+import React, { useEffect, useRef } from 'react'
+import canvasState from '../store/canvasState';
+import toolState from '../store/toolState';
 import '../styles/canvas.scss'
+import Brush from '../tools/Brush';
 
-const Canvas = () => {
+const Canvas = observer(() => {
+    const canvasRef = useRef()
+
+    useEffect(() => {
+        canvasState.setCanvas(canvasRef.current)
+        toolState.setTool(new Brush(canvasRef.current))
+    }, [])
+
     return (
         <div className='canvas'>
-            <canvas width={600} height={400} />
+            <canvas ref={canvasRef} width={600} height={400} />
         </div>
-    );
-};
+    )
+})
 
 export default Canvas;
--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -4,9 +4,17 @@ import '../styles/toolbar.scss'
 const ToolBar = () => {
     return (
         <div className='toolbar'>
-            
+            <button className='toolbar__btn brush'></button>
+            <button className='toolbar__btn rect'></button>
+            <button className='toolbar__btn circle'></button>
+            <button className='toolbar__btn eraser'></button>
+            <button className='toolbar__btn line'></button>
+            <input style={{marginLeft: 10}} type="color" />
+            <button className='toolbar__btn undo'></button>
+            <button className='toolbar__btn redo'></button>
+            <button className='toolbar__btn save'></button>
         </div>
-    );
-};
+    )
+}
 
-export default ToolBar;
+export default ToolBar
